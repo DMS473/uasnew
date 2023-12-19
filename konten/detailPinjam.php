174c3d0351122a3fb14207daaf432e909148b64e
DMS473 <daffamuis0407@gmail.com>
2023-12-19 09:21:00 +0700

    add sql file

--- konten/detailPinjam.php
+++ konten/detailPinjam.php
@@ -1,24 +1,42 @@
 <?php  
     include "koneksi.php";
-    $id = $_GET['id'];
-    $query = "SELECT * FROM users where id = ". $_GET["id"]. ";";
+    $id1 = $_GET['id'];
+    $query = "select users.namadep, users.id, books.judul, books.id, pinjams.start, pinjams.isFinished from users join pinjams on users.id = pinjams.user_id join books on books.id = pinjams.book_id where pinjams.id = ". $_GET["id"]. ";";
+    
+    //  = 6;
     // $query = "SELECT * FROM register";
     // echo $query;
     $sql = mysqli_query($conn, $query);
 
     $result = mysqli_fetch_assoc($sql);
     // var_dump($result);
+    // $timenow = time();
+    $status = $result['isFinished'];
+    if ($status == 1) {
+        $status2 = "selesai";
+    } else {
+        $status2 = "belum selesai";
+    }
 
+    $haripinjam = $result['start'];
+    $f=strtotime($haripinjam);
+    $d2=ceil(($f-time())/60/60/24);
+    $biaya=0;
+    if($d2>3){
+        $biaya= ($d2-3)*500;
+    } else {
+        $biaya= 0;
+    }
 
     // echo "text : " . $_GET['id'];
 ?>
 
 
 <!-- ini halaman home -->
-<form action="updateUser.php" method="post">
+<form action="selesaiPinjam.php" method="post">
     <p>&nbsp;</p>
     <div align="center" class="style1">
-        <p>EDIT</p>
+        <p>DETAIL PINJAM</p>
     </div>
     <!-- cari Member
     <from action="module/cari.php" method="get"> 
@@ -31,58 +49,35 @@
         // while($result = mysqli_fetch_assoc($sql)){
     ?>
         <tr>
-            <td width="163">Nama depan :</td>
-            <td width="317"><input type="text" name="namadep" value="<?php echo $result['namadep']; ?>" /></td>
+            <td width="163">Nama Peminjam :</td>
+            <td width="317"><input type="text" name="namadep" value="<?php echo $result['namadep']; ?>" readonly/></td>
         </tr>
         <tr>
-            <td>Nama Belakang :</td>
-            <td><input type="text" name="namabel" value="<?php echo $result['namabel']; ?>" /></td>
+            <td>NIM Peminjam :</td>
+            <td><input type="text" name="namabel" value="<?php echo $result['id']; ?>" readonly/></td>
         </tr>
         <tr>
-            <td>Username :</td>
-            <td><input type="text" name="username" value="<?php echo $result['username']; ?>"/></td>
+            <td>Judul Buku yang dipinjam :</td>
+            <td><input type="text" name="username" value="<?php echo $result['judul']; ?>" readonly/></td>
         </tr>
         <tr>
-            <td>Password :</td>
-            <td><input type="password" name="password" value="<?php echo $result['password']; ?>"/></td>
+            <td>Kode buku :</td>
+            <td><input type="text" name="password" value="<?php echo $result['id']; ?>" readonly/></td>
+        </tr>
+       
+        <tr>
+            <td>Tanggal Meminjam :</td>
+            <td><input type="text" name="usia" value="<?php echo $result['start']; ?>" readonly/></td>
+        </tr>
+        <tr>
+            <td>Denda :</td>
+            <td><input type="text" name="usia" value="Rp. <?php echo $biaya; ?>" readonly/></td>
+        </tr>
+        <tr>
+            <td>Status :</td>
+            <td><input type="text" name="usia" value="<?php echo $status2; ?>" readonly/></td>
         </tr>
         <!-- <tr>
-            <td>Usia :</td>
-            <td><input type="text" name="usia" value="<?php echo $result['usia']; ?>"/></td>
-        </tr> -->
-        <tr>
-            <td>Role :</td>
-            <td>
-            <input type="radio" id="admin" name="role" value="Admin">
-            <label for="admin">Admin</label><br>
-            </td>
-        </tr>
-        <tr>
-            <td>&nbsp;</td>
-            <td>
-            <input type="radio" id="dekan" name="role" value="Dekan">
-            <label for="dekan">Dekan</label><br>
-            </td>
-        </tr>
-        <tr>
-            <td>&nbsp;</td>
-            <td>
-            <input type="radio" id="operator" name="role" value="Operator">
-            <label for="operator">Operator</label><br>
-            </td>
-        </tr>
-        <tr>
-            <td>&nbsp;</td>
-            <td>
-            <input type="radio" id="mahasiswa" name="role" value="Mahasiswa">
-            <label for="mahasiswa">Mahasiswa</label><br>
-            </td>
-        </tr>
-        <tr>
-            <td>Usia :</td>
-            <td><input type="text" name="usia" value="<?php echo $result['usia']; ?>"/></td>
-        </tr>
-        <tr>
             <td>Jenis Kelamin :</td>
             <td>
             <input type="radio" id="pria" name="jk" value="pria">
@@ -95,18 +90,26 @@
             <input type="radio" id="wanita" name="jk" value="wanita">
             <label for="wanita">Wanita</label><br>
             </td>
-        </tr>
+        </tr> -->
         <!-- <tr>
             <td>Nomor Telepon :</td>
             <td><input type="text" name="notel" value="<?php echo $result['notel']; ?>"/></td>
         </tr>
         <tr>
             <!-- <td>id :</td> -->
-            <td><input type="hidden" name="id" value="<?php echo $result['id']; ?>"/></td>
+            <td><input type="hidden" name="id" value="<?php echo $id1; ?>"/></td>
         </tr>
         <tr>
             <td>&nbsp;</td>
-            <td><input type="submit" value="update" /></td>
+            <?php 
+                if($status==1){
+                    echo "<td><input type='submit' value='lunas' disabled/></td>";
+                } else {
+                    echo "<td><input type='submit' value='lunas' /></td>";
+                };
+
+            ?>
+            <!-- <td><input type="submit" value="lunas" /></td> -->
         </tr>
 
     <?php
